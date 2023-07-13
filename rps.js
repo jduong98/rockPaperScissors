87783483370a4ccf379337296b991801d4dec240
JM <132615501+jduong98@users.noreply.github.com>
2023-07-13 11:57:29 -0400

    Create functions and logic for game to work

--- rps.js
+++ rps.js
@@ -4,49 +4,121 @@ function getComputerChoice() {
 
 }
 
-function caseInsensitive(string) {
-    firstLetter = string.charAt(0).toUpperCase()
-    rest = string.slice(1).toLowerCase()
-    return firstLetter+rest
+function playRound(playerChoice, computerChoice) {
+  // 1 means player wins
+  // 2 means computer wins
+  
+  switch (playerChoice) {
+      case computerChoice:
+        // return "Draw!";
+        return 0;
+      case "Rock":
+        if (computerChoice === "Paper") {
+          // return "Computer wins";
+          return 2;
+        } else if (computerChoice === "Scissors") {
+          // return "Player wins";
+          return 1;
+        }
+        break;
+
+
+      case "Paper":
+        if (computerChoice === "Rock") {
+          // return "Player wins";
+          return 1;
+        } else if (computerChoice === "Scissors") {
+          // return "Computer wins";
+          return 2;
+        }
+        break;
+
+
+      case "Scissors":
+        if (computerChoice === "Paper") {
+          // return "Player wins";
+          return 1;
+        } else if (computerChoice === "Rock") {
+          // return "Computer Wins";
+          return 2;
+        }
+        break;
+    }
+  }
+// function caseInsensitive(string) {
+//     firstLetter = string.charAt(0).toUpperCase()
+//     rest = string.slice(1).toLowerCase()
+//     return firstLetter+rest
+
+// }
+
+let playerScore = 0;
+let computerScore = 0;
+let result;
+let pScore = document.getElementById('player');
+let cScore = document.getElementById('computer');
+let res = document.getElementById('result')
+let count = document.getElementById('counter')
+pScore.textContent = "Player Score is: " + playerScore;
+cScore.textContent = "Computer Score is: " + computerScore;
+let counter = 0;
+
+
+// let computerChoice = "Paper";
+const buttons = document.querySelectorAll('button');
+
+
+// make function to check the result and put a choice 
+function check(result){
+  if (result == 1){
+    result = "Player Won!";
+    res.textContent = result;
+    playerScore++;
+    pScore.textContent = "Player Score is: " + playerScore;
+    cScore.textContent = "Computer Score is: " + computerScore;
+    
+  } 
+  else if(result == 2){
+    result = "Computer Won!";
+    res.textContent = result;
+    computerScore++;
+    pScore.textContent = "Player Score is: " + playerScore;
+    cScore.textContent = "Computer Score is: " + computerScore;
+
+  }else{
+    result = "Draw!";
+    res.textContent = result;
+
+  }
+
 
 }
 
 
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    let computerChoice = getComputerChoice()
+    console.log(computerChoice)
+    playerChoice = button.value;
+    result = playRound(playerChoice, computerChoice)
+    check(result)
+    ++counter;
+    
+  });
+});
 
-computerChoice = getComputerChoice()
 
-playerChoice = caseInsensitive(prompt("Rock, Paper, or Scissors? "))
 
-console.log(playerChoice)
+// playerChoice = caseInsensitive(prompt("Rock, Paper, or Scissors? "))
 
-function playRound(playerChoice, computerChoice) {
-    switch (playerChoice) {
-        case computerChoice:
-          return "Draw!";
-        case "Rock":
-          if (computerChoice === "Paper") {
-            return "Player loses";
-          } else if (computerChoice === "Scissors") {
-            return "Player wins";
-          }
-          break;
-        case "Paper":
-          if (computerChoice === "Rock") {
-            return "Player wins";
-          } else if (computerChoice === "Scissors") {
-            return "Player loses";
-          }
-          break;
-        case "Scissors":
-          if (computerChoice === "Paper") {
-            return "Player wins";
-          } else if (computerChoice === "Rock") {
-            return "Player loses";
-          }
-          break;
-      }
-    }
+// console.log(playerChoice)
 
-result = playRound(playerChoice, computerChoice)
 
-alert(result)
+// count.textContent = counter;
+
+
+// cScore.textContent = "Computer Score is: " + computerScore;
+// result = playRound(playerChoice, computerChoice)
+
+// console.log(result)
