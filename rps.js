32fe585700b143987921696273d55062af2cd47c
JM <132615501+jduong98@users.noreply.github.com>
2023-07-14 09:32:16 -0400

    Add reset function and disable buttons after win

--- rps.js
+++ rps.js
@@ -5,6 +5,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
+  // 0 means draw
   // 1 means player wins
   // 2 means computer wins
   
@@ -45,6 +46,57 @@ function playRound(playerChoice, computerChoice) {
         break;
     }
   }
+
+  function check(result){
+    if (result == 1){
+      result = "Player Won!";
+      res.textContent = result;
+      playerScore++;
+      pScore.textContent = "Player Score is: " + playerScore;
+      cScore.textContent = "Computer Score is: " + computerScore;
+    } 
+    else if(result == 2){
+      result = "Computer Won!";
+      res.textContent = result;
+      computerScore++;
+      pScore.textContent = "Player Score is: " + playerScore;
+      cScore.textContent = "Computer Score is: " + computerScore;
+    }else{
+      result = "Draw!";
+      res.textContent = result;
+    }
+  }
+
+function announceWinner(player, computer){
+    let text = "Final Winner: "
+    if(player == 5){
+      res.textContent = text + "Player";
+    const rock = document.querySelector('#rock').disabled =  true;
+    const paper = document.querySelector('#paper').disabled =  true;
+    const scissor = document.querySelector('#scissor').disabled =  true;
+    } else if (computer == 5){
+      res.textContent = text + "Computer";
+      const rock = document.querySelector('#rock').disabled =  true;
+      const paper = document.querySelector('#paper').disabled =  true;
+      const scissor = document.querySelector('#scissor').disabled =  true;
+        }
+}
+
+
+
+function reset(){
+    playerScore = 0;
+    computerScore = 0;
+    counter = 0;
+    result = "";
+    res.textContent = result;
+    pScore.textContent = "Player Score is: " + playerScore;
+    cScore.textContent = "Computer Score is: " + computerScore;
+    count.textContent = "Round: "+ counter;
+    const rock = document.querySelector('#rock').disabled =  false;
+    const paper = document.querySelector('#paper').disabled =  false;
+    const scissor = document.querySelector('#scissor').disabled =  false;
+}
 // function caseInsensitive(string) {
 //     firstLetter = string.charAt(0).toUpperCase()
 //     rest = string.slice(1).toLowerCase()
@@ -54,7 +106,7 @@ function playRound(playerChoice, computerChoice) {
 
 let playerScore = 0;
 let computerScore = 0;
-let result;
+let result = '';
 let pScore = document.getElementById('player');
 let cScore = document.getElementById('computer');
 let res = document.getElementById('result')
@@ -62,37 +114,17 @@ let count = document.getElementById('counter')
 pScore.textContent = "Player Score is: " + playerScore;
 cScore.textContent = "Computer Score is: " + computerScore;
 let counter = 0;
-count.textContent = counter;
+count.textContent = "Round: "+ counter;
 
 // let computerChoice = "Paper";
-const buttons = document.querySelectorAll('button');
-
-
+const buttons = document.querySelectorAll('.choice');
 // make function to check the result and put a choice 
-function check(result){
-  if (result == 1){
-    result = "Player Won!";
-    res.textContent = result;
-    playerScore++;
-    pScore.textContent = "Player Score is: " + playerScore;
-    cScore.textContent = "Computer Score is: " + computerScore;
-    
-  } 
-  else if(result == 2){
-    result = "Computer Won!";
-    res.textContent = result;
-    computerScore++;
-    pScore.textContent = "Player Score is: " + playerScore;
-    cScore.textContent = "Computer Score is: " + computerScore;
 
-  }else{
-    result = "Draw!";
-    res.textContent = result;
+const resetBtn = document.querySelector(".reset")
 
-  }
-
-
-}
+resetBtn.addEventListener("click", () => {
+  reset();
+})
 
 
 buttons.forEach((button) => {
@@ -103,12 +135,16 @@ buttons.forEach((button) => {
     playerChoice = button.value;
     result = playRound(playerChoice, computerChoice)
     check(result)
+    announceWinner(playerScore, computerScore)
     ++counter;
     count.textContent = counter;
   });
 });
 
 
+
+
+// TODO: 
 
 // playerChoice = caseInsensitive(prompt("Rock, Paper, or Scissors? "))
 
